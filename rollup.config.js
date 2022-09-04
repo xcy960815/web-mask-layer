21379e0258b96f0c90b23fe75aaadc821fbfd4fa
xuchongyu <xuchongyu@weidian.com>
2022-09-04 12:51:25 +0800

    初始化+调整

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,48 @@
 import typescript from 'rollup-plugin-typescript2'
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import {
+    babel
+} from '@rollup/plugin-babel'
+import del from 'rollup-plugin-delete'
 import {
     terser
 } from 'rollup-plugin-terser'
-import {
-    nodeResolve
-} from '@rollup/plugin-node-resolve' //将外部引入的js打包进来
-import babel from 'rollup-plugin-babel'
-import del from 'rollup-plugin-delete' //
-import commonjs from '@rollup/plugin-commonjs' //将CommonJS模块转换为ES6, 方便rollup直接调用
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
 import livereload from 'rollup-plugin-livereload'
+
 const isProduction = process.env.NODE_ENV === 'production'
 export default {
-    input: './src/index.ts',
-    output: [{
-            format: 'umd',
-            file: 'dist/index.umd.js',
-            name: 'example',
-        },
+    input: './src/mask-layer.ts',
+    output: [
         {
             format: 'umd',
-            file: 'demo/index.umd.js',
-            name: 'example',
+            file: 'dist/web-mask-layer.umd.js',
+            name: 'webMaskLayer',
         },
-        // {
-        //     format: 'cjs',
-        //     file: 'dist/index.cjs.js',
-        // },
         {
             format: 'es',
-            file: 'dist/index.esm.js',
+            file: 'dist/web-mask-layer.esm.js',
         },
     ],
     plugins: [
-        del({
-            targets: ['dist', 'demo/index.umd.js']
-        }),
-        nodeResolve(),
-        commonjs({
-            include: 'node_modules/**',
+        isProduction && del({
+            targets: ['dist']
         }),
         isProduction && terser(),
         babel({
             exclude: 'node_modules/**',
+            babelHelpers: 'bundled',
+        }),
+        postcss({
+            plugins: [autoprefixer()],
         }),
         // 热更新
         !isProduction && livereload(),
+
         typescript({
             exclude: 'node_modules/**',
             useTsconfigDeclarationDir: true,
             extensions: ['.js', '.ts', '.tsx'],
         }),
-        sourceMaps(),
     ],
 }
