1388a5eca2360f2940c0eaa2ef9546c2ef64c72c
xcy960815 <18763006837@163.com>
2022-09-04 12:51:25 +0800

    初始化+调整

---
+++ example/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <div class='web-mask-layer'> -->
+  <!-- </div> -->
+</template>
+
+<script lang='ts'>
+import {MaskLayer} from "web-mask-layer";
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    //  创建一个遮罩层
+    createMaskLayer() {
+      const maskLayer = new MaskLayer();
+      maskLayer.createLoading("加载中...");
+    }
+  },
+  mounted() {
+    this.createMaskLayer();
+  }
+});
+</script>
+<style lang='less' scoped>
+.web-mask-layer {
+  position: relative;
+}
+</style>
---
+++ temp/maskLayer.d.ts
@@ -0,0 +1,57 @@
+interface MaskLayerOptions {
+    loadingText: string;
+}
+export declare class MaskLayer {
+    private maskLayerElement;
+    /**
+     * 遮罩层队列*
+     */
+    readonly maskLayerQueue: Array<MaskLayerOptions>;
+    /**
+     * @desc 更新遮罩层内容
+     * @param {MaskLayerOptions} maskLayerOptions 遮罩层配置项
+     * @return {void}
+     */
+    private uploadMaskLayerText;
+    /**
+     * @desc 创建或者更新遮罩层
+     * @return {void}
+     */
+    private uploadMaskLayer;
+    /**
+     * @desc 向遮罩层队列中添加记录
+     * @param {MaskLayerOptions} maskLayerOptions
+     * @return {void}
+     */
+    private addMaskLayerLayer;
+    /**
+     * @desc 创建节点方法
+     * @param {string} tagName
+     * @param {string} className
+     * @param {string} innerText
+     * @return HTMLElement
+     * @private
+     */
+    private createElement;
+    /**
+     * @desc 创建遮罩层节点
+     * @param {MaskLayerOptions} maskLayerOptions
+     * @private
+     * @return {void}
+     */
+    private createLoadingElement;
+    /**
+     * @desc 移除遮罩层
+     */
+    private removeLoadingElement;
+    /**
+     * @desc 创建遮罩层
+     * @param loadingText
+     */
+    createLoading(loadingText: string): void;
+    /**
+     * @desc 关闭遮罩层
+     */
+    removeLoading(): void;
+}
+export {};
